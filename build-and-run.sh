2c83876afd0e921f507473f7a501f7ffe8de497d
K3rnelPan1c <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2021-12-20 16:01:31 +0100

    update docs for new unprivileged container port
    Signed-off-by: K3rnelPan1c <69395733+k3rnelpan1c-dev@users.noreply.github.com>

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 mkdir --parents repo
-mkdir --parents bin
 docker build --tag localhost/cyclonedx-bom-repo-server .
-docker run --volume "$(pwd)/repo":/repo --env REPO__DIRECTORY=/repo --env ALLOWEDMETHODS__GET="true" --env ALLOWEDMETHODS__POST="true" --env ALLOWEDMETHODS__DELETE="true" --tty --interactive -p 8000:80 cyclonedx-bom-repo-server
+docker run --volume "$(pwd)/repo":/repo --env REPO__DIRECTORY=/repo --env ALLOWEDMETHODS__GET="true" --env ALLOWEDMETHODS__POST="true" --env ALLOWEDMETHODS__DELETE="true" --tty --interactive -p 8000:8080 cyclonedx-bom-repo-server
