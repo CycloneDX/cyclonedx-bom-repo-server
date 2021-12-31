06bfcb3c71c7b4994fb2cab38ba8a511e3876bc6
Magnus Persson <magnus.e.persson@gmail.com>
2021-12-31 09:02:53 +0100

    Work on tests
    Signed-off-by: Magnus Persson <magnus.e.persson@gmail.com>

--- src/CycloneDX.BomRepoServer/Services/S3RepoService.cs
+++ src/CycloneDX.BomRepoServer/Services/S3RepoService.cs
@@ -28,16 +28,20 @@ using CycloneDX.BomRepoServer.Options;
 using CycloneDX.Models.v1_3;
 using Microsoft.Extensions.Logging;
 using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace CycloneDX.BomRepoServer.Services
 {
     class S3RepoService : IRepoService
     {
         private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+        private const int InternalStorageVersion = 1;
 
         public S3RepoService(IAmazonS3 s3Client)
         {
             this._s3Client = s3Client;
+            this._bucketName = "bomserver"; // TODO Configurable
         }
 
         public void Delete(string serialNumber, int version)
@@ -52,16 +56,49 @@ namespace CycloneDX.BomRepoServer.Services
 
         public IEnumerable<string> GetAllBomSerialNumbers()
         {
-            
-            var s3objects = _s3Client.ListObjectsAsync("bomserver").GetAwaiter().GetResult();
-            return s3objects.S3Objects.Select(s3Object => {
-                return s3Object.Key;
-            });
+            return _s3Client
+                .Paginators
+                .ListObjectsV2(new ListObjectsV2Request{
+                    BucketName = "bomserver"
+                })
+                .S3Objects
+                .Where(s3Object => s3Object.Key.StartsWith("urn_uuid_"))
+                .Select(s3Object => s3Object.Key.Replace("_", ":"))
+                .ToEnumerable();
+        }
+
+        private string BomBaseDirectory()
+        {
+            return $"v{InternalStorageVersion}";
+        }
+
+        private string ReplaceInvalidFilepathSegmentCharacters(string filePathSegment)
+        {
+            // The only invalid character possible is ":" in serial number
+            return filePathSegment.Replace(':', '_');
+        }
+
+        private string BomInstanceBaseDirectory(string serialNumber)
+        {
+            return $"{BomBaseDirectory()}/{ReplaceInvalidFilepathSegmentCharacters(serialNumber)}";
         }
 
         public IEnumerable<int> GetAllVersions(string serialNumber)
         {
-            throw new NotImplementedException();
+            var instanceDirname = BomInstanceBaseDirectory(serialNumber);
+            return _s3Client
+                .Paginators
+                .ListObjectsV2(new ListObjectsV2Request{
+                    BucketName = this._bucketName,
+                    Prefix = instanceDirname,
+                })
+                .CommonPrefixes
+                .Select((commonPrefix) => {
+                    int.TryParse(commonPrefix, out int result);
+                    return result;
+                })
+                .ToEnumerable();
+            
         }
 
         public DateTime GetBomAge(string serialNumber, int version)
@@ -89,14 +126,87 @@ namespace CycloneDX.BomRepoServer.Services
             throw new NotImplementedException();
         }
 
+        private string BomDirectory(string serialNumber, int version)
+        {
+            return $"{BomInstanceBaseDirectory(serialNumber)}/{version.ToString()}";
+        }
+
+        private string BomFilename(string serialNumber, int version)
+        {
+            return $"{BomDirectory(serialNumber, version)}/bom.cdx";
+        }
+
+        private string OriginalBomFilename(string serialNumber, int version, Format format, SpecificationVersion specificationVersion)
+        {
+            return $"{BomDirectory(serialNumber, version)}/bom.{specificationVersion}.{format.ToString().ToLowerInvariant()}";
+            //return _fileSystem.Path.Combine(BomDirectory(serialNumber, version), $"bom.{specificationVersion}.{format.ToString().ToLowerInvariant()}");
+        }
+
         public Bom Store(Bom bom)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(bom.SerialNumber)) bom.SerialNumber = "urn:uuid:" + Guid.NewGuid();
+
+            if (!bom.Version.HasValue)
+            {
+                var latestVersion = GetLatestVersion(bom.SerialNumber);
+                if (latestVersion.HasValue)
+                {
+                    bom.Version = latestVersion.Value + 1;
+                }
+                else
+                {
+                    bom.Version = 1;
+                }
+            }
+
+            var directoryName = BomDirectory(bom.SerialNumber, bom.Version.Value);
+            var fileName = BomFilename(bom.SerialNumber, bom.Version.Value);
+            
+            using (var stream = new MemoryStream()) {
+                Protobuf.Serializer.Serialize(bom, stream);
+                _s3Client.PutObjectAsync(new PutObjectRequest{
+                    BucketName = this._bucketName,
+                    Key = fileName,
+                    InputStream = stream,
+                    ContentType = "application/protobuf" // TODO What content type?
+                })
+                .GetAwaiter()
+                .GetResult();
+            }
+
+            return bom;
+        }
+
+        private int? GetLatestVersion(string serialNumber)
+        {
+            var versions = GetAllVersions(serialNumber);
+            return versions.LastOrDefault();
         }
 
         public Task StoreOriginal(string serialNumber, int version, Stream bomStream, Format format, SpecificationVersion specificationVersion)
         {
-            throw new NotImplementedException();
+            var directoryName = BomDirectory(serialNumber, version);
+            var fileName = OriginalBomFilename(serialNumber, version, format, specificationVersion);
+
+            // TODO Check if object already exists
+            return _s3Client.PutObjectAsync(new PutObjectRequest() {
+                BucketName = this._bucketName,
+                InputStream = bomStream,
+                Key = fileName,
+                ContentType = "application/protobuf" // TODO Correct format
+            });
+            /*try
+            {
+                using var fs = _fileSystem.File.Open(fileName, System.IO.FileMode.CreateNew,
+                    System.IO.FileAccess.Write);
+                await bomStream.CopyToAsync(fs);
+            }
+            catch (System.IO.IOException)
+            {
+                if (_fileSystem.File.Exists(fileName))
+                    throw new BomAlreadyExistsException();
+                throw;
+            }*/
         }
     }
 }
