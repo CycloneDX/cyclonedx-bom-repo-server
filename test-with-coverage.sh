ed3ea7fd372c5cfd9b938dbed6cd0fb0f9758e05
Magnus Persson <magnus.e.persson@gmail.com>
2022-04-22 12:57:39 +0200

    Satisfy shellcheck
    Signed-off-by: Magnus Persson <magnus.e.persson@gmail.com>

--- test-with-coverage.sh
+++ test-with-coverage.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./BuildReports
 dotnet test --results-directory ./BuildReports --collect "XPlat Code Coverage"
-reportgenerator -reports:`find ./BuildReports -name coverage.cobertura.xml` -targetdir:BuildReports/Coverage -reporttypes:"HTML;HTMLSummary"
+reportgenerator -reports:"$(find ./BuildReports -name coverage.cobertura.xml)" -targetdir:BuildReports/Coverage -reporttypes:"HTML;HTMLSummary"
